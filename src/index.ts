b958c67da65b4d0129110f093e4f74a6b6083fb6
Thomas Schlender <thebuoyant@gmail.com>
2024-11-03 20:12:21 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+export function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+// Beispiel einer weiteren Utility-Funktion
+export function greet(name: string): string {
+  return `Hello, ${name}!`;
+}
