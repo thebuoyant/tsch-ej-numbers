4adf8f9b5ab95aee6efbb403743502087c4eb926
Thomas Schlender <thebuoyant@gmail.com>
2024-11-04 19:14:16 +0100

    EN-3 Clean tests

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,6 @@
 import { readJsonData } from "./handlers/record.handler";
 import { TRecord } from "./types/record.types";
 
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
-export function subtract(a: number, b: number): number {
-  return a - b;
-}
-
-// Beispiel einer weiteren Utility-Funktion
-export function greet(name: string): string {
-  return `Hello, ${name}!`;
-}
-
 export function getRecords(limit = 0): TRecord[] {
   return readJsonData(limit);
 }
