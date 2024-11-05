dd04a0d6cca194a5e6b0823f7c58378ded26abd6
Thomas Schlender <thebuoyant@gmail.com>
2024-11-05 19:34:42 +0100

    EN-10 Add api method

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import { getAllClassOneData } from "./handlers/record-class-one.handler";
 import { getFirstDrawData } from "./handlers/record-first-draw.handler";
 import { getLastDrawData } from "./handlers/record-last-draw.handler";
 import { getMaxJackpot } from "./handlers/record-max-jackpot.handler";
+import { getWinningNumbersCount } from "./handlers/record-winning-numbers-count.handler";
 import { readJsonData } from "./handlers/record.handler";
 import { TRecord } from "./types/record.types";
+import { TWinningNumbersKeyValue } from "./types/winning-numbers-key-value.types";
 
 export function getRecords(limit = 0): TRecord[] {
   return readJsonData(limit);
@@ -23,4 +25,10 @@ export function getClassOneDraws(limit = 0): TRecord[] {
 
 export function getMaxJackpotDraws(limit = 0): TRecord[] {
   return getMaxJackpot(limit);
+}
+
+export function getDrawsWinningNumbersCount(
+  limit = 0
+): TWinningNumbersKeyValue[] {
+  return getWinningNumbersCount(limit);
 }
